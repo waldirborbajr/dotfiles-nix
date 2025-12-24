1b2bc4d0c88e82f3bb521063dc44ddff76479891
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-23 21:30:13 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,8 @@
 {
-  description = "NixOS configuration (borba)";
+  description = "Borba NixOS flake";
 
-
-
-  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,13 +17,13 @@
         inherit system;
 
         modules = [
-          ./nixos/configuration.nix
+          ./hosts/caveos/configuration.nix
 
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-	    home-manager.backupFileExtension = "backup";
+            home-manager.backupFileExtension = "backup";
 
             home-manager.users.borba = import ./home/home.nix;
           }
