a0af47bebec08d5345f47f54851639aa6cea33fd
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-23 21:14:33 -0300

    Update wezterm.nix

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -9,7 +9,7 @@ local wezterm = require 'wezterm'
 return {
   default_prog = { "zsh" },
   hide_tab_bar_if_only_one_tab = true,
-  color_scheme = "Gruvbox Dark",
+  color_scheme = "Catppuccin Mocha",
   front_end = "WebGpu",
 }
 '';
