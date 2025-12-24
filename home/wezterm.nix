8c0df815253bc3ce00b369b937709397bd643e82
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-23 21:02:55 -0300

    Create wezterm.nix

---
+++ home/wezterm.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ pkgs.wezterm ];
+
+  home.file.".config/wezterm/wezterm.lua".text = ''
+local wezterm = require 'wezterm'
+
+return {
+  default_prog = { "zsh" },
+  hide_tab_bar_if_only_one_tab = true,
+  color_scheme = "Gruvbox Dark",
+  front_end = "WebGpu",
+}
+'';
+}
