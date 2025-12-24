43398668b8b554f9d77559c46c12ea124ef26e8b
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-24 08:40:32 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   description = "Borba NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      #url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
