3d4e8413c231ae86993fbabf51b6632d9e5d029c
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-24 08:34:23 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,29 +2,7 @@
   description = "Borba NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations.caveos = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        modules = [
-          ./hosts/caveos/configuration.nix
-
-          home-manager.nixosModules.default{
-  description = "Borba NixOS flake";
-
-  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,19 +20,6 @@
           ./hosts/caveos/configuration.nix
 
           home-manager.nixosModules.default
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
-
-            home-manager.users.borba = import ./home/home.nix;
-          }
-        ];
-      };
-    };
-}
-
-
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
