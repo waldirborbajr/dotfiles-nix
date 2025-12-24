bf82df5c8df905b375a44bfeba893cd6116a8bb7
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-24 14:23:32 +0000

    wezterm

--- home/wezterm/wezterm.nix
+++ home/wezterm/wezterm.nix
@@ -1,16 +1,8 @@
-{ pkgs, ... }:
+{ builtins }:
 
 {
-  home.packages = [ pkgs.wezterm ];
-
-  home.file.".config/wezterm/wezterm.lua".text = ''
-    local wezterm = require 'wezterm'
-
-    return {
-      default_prog = { "zsh" },
-      hide_tab_bar_if_only_one_tab = true,
-      color_scheme = "Catppuccin Mocha",
-      front_end = "WebGpu",
-    }
-  '';
+  programs.wezterm = {
+    enable = true;
+    extraConfig = import ./settings.nix { inherit builtins; };
+  };
 }
