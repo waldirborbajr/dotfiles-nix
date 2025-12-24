30ce1d8cb40558434b401a33e33b863da3e6a94c
BORBA JUNIOR, W - PU5MHL <wborbajr@gmail.com>
2025-12-24 09:48:24 -0300

    Update helix.nix

--- home/helix.nix
+++ home/helix.nix
@@ -7,38 +7,8 @@
       # Nix
       nil
       nixpkgs-fmt
-      # Web
-      vscode-langservers-extracted # HTML/CSS/JSON/ESLint language servers
-      typescript-language-server
-      svelte-language-server
-      nodePackages.prettier
-      nodePackages.eslint
-      # Markdown
-      marksman
-      markdownlint-cli
-      # Lua
-      lua-language-server
-      # Python
-      pyright
-      ruff-lsp
-      # C#
-      csharp-ls
-      # C, C++
       clang-tools
-      # Rust
-      rust-analyzer
-      rustfmt
-      # SQL
-      sqls
-      # Haskell
-      haskell-language-server
-      # Bash
-      bash-language-server
-      # TOML
-      taplo
       # Runtimes and Libraries
-      lldb
-      helix
     ];
   };
 
