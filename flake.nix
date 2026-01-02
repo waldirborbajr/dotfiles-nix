219b7d45bed2660769b60dcf7ad882bc55a1e400
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-01-02 13:12:21 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     nixpkgs-25.11.url = "github:NixOS/nixpkgs/nixos-25.11";
     
     home-manager = {
-      #url = "github:nix-community/home-manager/release-25.11";
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -17,7 +16,7 @@
     {
       self,
       nixpkgs,
-      home-manager,
+      inputs,
       ...
     }:
     let
